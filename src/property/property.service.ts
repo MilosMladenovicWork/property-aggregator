c0c96a94b8240c8899bf10cd2641393b8c237452
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2023-12-24 15:09:48 +0100

    feat(root): :sparkles: install puppeteer and scrap some data

--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { isNil } from 'lodash';
+import { Browser, launch } from 'puppeteer';
 
 @Injectable()
 export class PropertyService {
@@ -7,6 +9,59 @@ export class PropertyService {
     return 'Hello World!';
   }
 
-  @Cron(CronExpression.EVERY_MINUTE)
-  syncPropertiesFromExternalProviders() {}
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  async syncPropertiesFromExternalProviders() {
+    const browser = await launch();
+
+    const haloOglasiProperties = await this.getPropertiesFromHaloOglasi({
+      browser,
+    });
+
+    console.log(haloOglasiProperties);
+  }
+
+  async getPropertiesFromHaloOglasi({ browser }: { browser: Browser }) {
+    const page = await browser.newPage();
+
+    try {
+      await page.goto(
+        'https://www.halooglasi.com/nekretnine/prodaja-stanova/beograd',
+      );
+      const properties = await page.$$eval(
+        '.product-item:not(.banner-list)',
+        (elements) => {
+          return elements.slice(0, 100).map((element) => {
+            if (isNil(element)) {
+              return;
+            }
+
+            const title =
+              element.querySelector<HTMLElement>('h3.product-title')?.innerText;
+
+            const locationHtmlElements = element.querySelectorAll<HTMLElement>(
+              '.subtitle-places li',
+            );
+
+            const location = !isNil(locationHtmlElements)
+              ? Array.from(locationHtmlElements)
+              : [];
+
+            const image =
+              element.querySelector<HTMLElement>('img.resized-image');
+
+            return {
+              title,
+              location,
+              image,
+            };
+          });
+        },
+      );
+      console.log('Properties', properties);
+    } catch (error) {
+      console.error('Error while scraping job listings:', error);
+    } finally {
+      await browser.close();
+    }
+  }
 }
