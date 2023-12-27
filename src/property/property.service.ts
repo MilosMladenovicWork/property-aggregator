9226a3fe3282c7747f7524a34fb82ea312ee25ab
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2023-12-27 20:52:24 +0100

    feat(puppeteer): :sparkles: start browser in headless mode

--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -1,19 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { Model } from 'mongoose';
 import { launch } from 'puppeteer';
 import { PropertyHaloOglasiService } from './property-halo-oglasi.service';
-import { Property } from './schema/property.schema';
 
 @Injectable()
 export class PropertyService {
   syncPropertiesFromExternalProvidersExecuting = false;
 
-  constructor(
-    @InjectModel(Property.name) private propertyModel: Model<Property>,
-    private propertyHaloOglasiService: PropertyHaloOglasiService,
-  ) {}
+  constructor(private propertyHaloOglasiService: PropertyHaloOglasiService) {}
 
   getHello(): string {
     return 'Hello World!';
@@ -25,7 +19,7 @@ export class PropertyService {
       if (!this.syncPropertiesFromExternalProvidersExecuting) {
         this.syncPropertiesFromExternalProvidersExecuting = true;
 
-        const browser = await launch({ headless: false });
+        const browser = await launch({ headless: true });
 
         await this.propertyHaloOglasiService.getPropertiesFromHaloOglasi({
           browser,
