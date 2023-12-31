025f3dd9c8cee5ffaf2ef47dc2246ab8032dc5d9
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2023-12-31 13:27:24 +0100

    feat(property-search-search-request-query): :sparkles: add length validation

--- src/property-search/dtos/property-search-search-request-query.dto.ts
+++ src/property-search/dtos/property-search-search-request-query.dto.ts
@@ -1,35 +1,51 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
+import {
+  PROPERTY_SEARCH_LOCATION_MAX_LENGTH,
+  PROPERTY_SEARCH_NUMBER_OF_ROOMS_MAX_LENGTH,
+  PROPERTY_SEARCH_PRICE_MAX_LENGTH,
+  PROPERTY_SEARCH_PROPERTY_FLAGS_MAX_LENGTH,
+  PROPERTY_SEARCH_QUERY_MAX_LENGTH,
+  PROPERTY_SEARCH_SQUARE_METERS_MAX_LENGTH,
+} from '../constants/property-search.constant';
 
 export class PropertySearchSearchRequestQuery {
   @IsString()
+  @MaxLength(PROPERTY_SEARCH_QUERY_MAX_LENGTH)
   @IsOptional()
   query?: string | null;
 
   @IsString()
+  @MaxLength(PROPERTY_SEARCH_LOCATION_MAX_LENGTH)
   @IsOptional()
   location?: string | null;
 
   @IsString()
+  @MaxLength(PROPERTY_SEARCH_PROPERTY_FLAGS_MAX_LENGTH)
   @IsOptional()
   propertyFlags?: string | null;
 
   @IsString()
+  @MaxLength(PROPERTY_SEARCH_NUMBER_OF_ROOMS_MAX_LENGTH)
   @IsOptional()
   numberOfRooms?: string | null;
 
   @IsString()
+  @MaxLength(PROPERTY_SEARCH_SQUARE_METERS_MAX_LENGTH)
   @IsOptional()
   squareMetersMin?: string | null;
 
   @IsString()
+  @MaxLength(PROPERTY_SEARCH_SQUARE_METERS_MAX_LENGTH)
   @IsOptional()
   squareMetersMax?: string | null;
 
   @IsString()
+  @MaxLength(PROPERTY_SEARCH_PRICE_MAX_LENGTH)
   @IsOptional()
   priceMin?: string | null;
 
   @IsString()
+  @MaxLength(PROPERTY_SEARCH_PRICE_MAX_LENGTH)
   @IsOptional()
   priceMax?: string | null;
 }
