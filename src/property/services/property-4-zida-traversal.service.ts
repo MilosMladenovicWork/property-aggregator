fa1a98bf72807176029717d86c557d9d16b44ac3
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-01-02 10:47:09 +0100

    feat(property-4-zida-traversal): :sparkles: create service for 4 zida property provider traversal

---
+++ src/property/services/property-4-zida-traversal.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { Page } from 'puppeteer';
+import { Property } from '../schema/property.schema';
+
+@Injectable()
+export class PropertyHaloOglasiTraversalService {
+  private propertyResultsUrl: string =
+    'https://www.4zida.rs/prodaja-stanova/beograd';
+
+  async goToSpecificPropertiesResultPage({
+    pageNumber,
+    page,
+  }: {
+    pageNumber: number;
+    page: Page;
+  }) {
+    const propertyResultsUrl = new URL(this.propertyResultsUrl);
+
+    propertyResultsUrl.searchParams.set('strana', pageNumber.toString());
+
+    await page.goto(propertyResultsUrl.toString());
+  }
+
+  async goToPropertyPage({
+    page,
+    property,
+  }: {
+    page: Page;
+    property: Property;
+  }) {
+    await page.goto(property.url);
+  }
+}
