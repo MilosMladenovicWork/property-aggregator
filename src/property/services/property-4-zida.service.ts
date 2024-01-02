af6e0bd782ba126c400e0744e1ec3323f33d0a98
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-01-02 10:45:05 +0100

    feat(property-4-zida): :sparkles: create root service for 4 zida property provider

---
+++ src/property/services/property-4-zida.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { InjectPage } from 'nest-puppeteer';
+import { Page } from 'puppeteer';
+import { Property } from '../schema/property.schema';
+
+@Injectable()
+export class PropertyHaloOglasiService {
+  constructor(
+    @InjectModel(Property.name) private propertyModel: Model<Property>,
+    //@ts-expect-error ts version problem
+    @InjectPage() private readonly page: Page,
+  ) {}
+}
